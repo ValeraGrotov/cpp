eb880c20b67a60dbc40fa19bc32a2a9163bf8d2d
ValeraGrotov <valeragrotov@gmail.com>
2020-10-18 19:34:41 +0300

    Update numbers.cpp

--- numbers.cpp
+++ numbers.cpp
@@ -5,39 +5,51 @@ using namespace std;
 
 int main() {
     setlocale(LC_ALL, "rus");
-   
-    int command;
-
-    int counter = 1;
-
-    vector<int> numbers;
-
-    int otvet = 1;
-
+  
     while (true) {
-        printf("Введите %i число: ", counter);
 
-        scanf_s("%i", &command);
+        int command;
+        int counter = 1;
+        vector<int> numbers;
+        int otvet = 1;
 
-        if (command == 0) {
-            break;
-        } else {
-            numbers.push_back(command);
+        while (true) {
+            printf("Введите %i число: ", counter);
 
-            for (int i = 0; i < counter; i++) {
-                if (numbers[i] > command) {
-                    otvet = 0;
+            scanf_s("%i", &command);
+
+            if (command == 0) {
+                if (counter == 1) {
+                    printf("Введите больше, чем 1 число");
+                    return 0;
                 }
+                break;
+            } else {
+                numbers.push_back(command);
+
+                for (int i = 0; i < counter; i++) {
+                    if (numbers[i] > command) {
+                        otvet = 0;
+                    }
+                }
+
+                counter++; // счетчик увеличиваем здесь
             }
-
-            counter++; // счетчик увеличиваем здесь
         }
-    }
-
-    if (otvet == 1) {
-        printf("Верно");        
-    } else {
-        printf("Неверно");        
+    
+        if (otvet == 1) {
+            printf("Верно");        
+        } else {
+            printf("Неверно");        
+        }
+    
+        printf("\n\nЖелаете повторить?\n\n1. Да\n2. Нет");
+        scanf_s("%i", &command);
+        if (command == 1) {
+            continue;
+        } else {
+            break;
+        }
     }
 
     return 0;
